57301b6b80e7e507d98b5717f761ae7acbd5de4d
okazuo <dellkazu@outlook.jp>
2021-09-30 16:42:58 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validatable :nickname, presence :true
-  validatable :last_namej, presence :true
-  validatable :first_namej, presence :true
-  validatable :last_namek, presence :true
-  validatable :first_namek, presence :true
-  validatable :birthday, presence :true
+  validates :nickname, presence: true
+  validates :last_namej, presence: true
+  validates :first_namej, presence: true
+  validates :last_namek, presence: true
+  validates :first_namek, presence: true
+  validates :birthday, presence: true
 
   has_many :items
 end
