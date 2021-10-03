399820850f5be5ad47067e381885bef6a4acac50
okazuo <dellkazu@outlook.jp>
2021-10-03 14:33:48 +0900

    ActiveHashの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,19 +1,26 @@
 class ItemsController < ApplicationController
   def index
+    @item = Item.order("created_at DESC")
   end
 
   def new
+    @item = Item.new
   end
 
   def create
-    @item = Item.create(item_params)
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   
   private
 
   def item_params
-    params.require(:item).permit(:title, :price, :explanation, :category_id, :status_id, :postage_id, :prefecture_id, :period_id).marge(user_id: current_user_id)
+    params.require(:item).permit(:title, :price, :explanation, :category_id, :status_id, :postage_id, :prefecture_id, :period_id, :image,).merge(user_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,15 @@
 class Item < ApplicationRecord
 
-  validates :title, presence: true
-  validates :price, presence: true
-  validates :explanation, presence: true
-  validates :category_id, presence: true
-  validates :status_id, presence: true
-  validates :postage_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :period_id, presence: true
+  validates :title, :price, :explanation, presence: true
+  validates :category_id, :status_id, :postage_id, :prefecture_id, :period_id, numericality: { other_than: 1, message: "can't be blank"}
   
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :postage
+  belongs_to :prefecture
+  belongs_to :period
 end
