2eae26bdf49dbb8a919876895df9673ca0aa1582
okazuo <okazuokakazu@gmail.com>
2021-10-16 17:36:08 +0900

    ログ表示の変更

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.16.0'
 
 # Capistranoのログの表示に利用する
-set :application, 'furimaapp'
+set :application, 'furima-36906'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:okazuo/furima-36906.git'
